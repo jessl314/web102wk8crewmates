68a79afb9b5fea6e744f709c29dace9e94cd1e03
jessl314 <jlchrysanthemum456@gmail.com>
2025-07-29 18:18:28 -0700

    updated home page

--- dcrew/src/pages/Home.jsx
+++ dcrew/src/pages/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import HumanoidSprites from "../assets/HumanoidSprites.png"
+import "./Home.css"
 
 const Home = () => {
     return (
-        <>
-        </>
+        <div className="home-container">
+            <div className="home">
+                <h1>Welcome to the D&D Party Builder</h1>
+                <h3>Here is where you can create your very own party of humanoids before sending them on their adventures!</h3>
+                <img src={HumanoidSprites} alt="a display of all possible D&D characters that can be chosen"/>
+                <br></br>
+                <p>credits to: https://deepdivegamestudio.itch.io/humanoid-asset-pack</p>
+
+            </div>
+        </div>
     )
 }
 
